c187ed6a8595b4d732b1b2bbe11528ad9a773835
Zhihao Cui <davidcui2@me.com>
2021-04-18 23:56:01 +0100

    Remove failed

--- scripts/github-actions.js
+++ scripts/github-actions.js
@@ -2,4 +2,4 @@ const core = require('@actions/core')
 
 core.warning('Some warning')
 core.setOutput('Output', 'value of output')
-core.setFailed('Oooops, something failed')
+// core.setFailed('Oooops, something failed')
